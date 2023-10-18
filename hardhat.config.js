f8b77325149c96276d7deabbbda92afdebf9a7dd
Gurudeep25 <gurukp02@gmail.com>
2023-10-18 21:59:17 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 
-
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.4",
   networks: {
     hardhat: {
       chainId: 1337
     },
-  }
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+     polygon: {
+        url: "https://polygon-mainnet.infura.io/v3/d94ead8d02104f0c8d4dae594216234d",
+        accounts: [process.env.pk]
+      }
+   }
 };
